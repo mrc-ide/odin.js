d26270a17c2c6248d9e4c463fcded10ca6ab3a6b
Rich FitzJohn <rich.fitzjohn@gmail.com>
2019-03-29 21:12:01 +0000

    Test wrapper behaviour

---
+++ tests/testthat/test-wrapper.R
@@ -0,0 +1,21 @@
+context("wrapper")
+
+
+test_that("force a vector of strings", {
+  gen <- odin_js(c("deriv(y) <- 0.5", "initial(y) <- 1"))
+  mod <- gen()
+  y <- mod$run(0:10)[, 1]
+  expect_equal(y, seq(1, by = 0.5, length.out = 11))
+})
+
+
+test_that("force a symbol of code", {
+  code <- quote({
+    deriv(y) <- 0.5
+    initial(y) <- 1
+  })
+  gen <- odin_js(code)
+  mod <- gen()
+  y <- mod$run(0:10)[, 1]
+  expect_equal(y, seq(1, by = 0.5, length.out = 11))
+})

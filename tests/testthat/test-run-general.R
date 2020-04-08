a66ac70c98ac229c51a43e15eb07b318d92a503f
Rich FitzJohn <rich.fitzjohn@gmail.com>
2020-04-08 18:03:30 +0100

    Something to fix later

--- tests/testthat/test-run-general.R
+++ tests/testthat/test-run-general.R
@@ -1,6 +1,9 @@
 context("run: general")
 
 test_that("conditionals, precendence", {
+  ## TODO: this fails if initial(x) is set to zero, like in the odin
+  ## test, due to integration not starting.  This is a general problem
+  ## in the underlying solver.
   gen <- odin_js({
     deriv(x) <- 0.1 + 2 * if (t > 2) -0.1 else 0.5
     initial(x) <- 1

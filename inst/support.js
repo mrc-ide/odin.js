319ed492bcb30fe808c5c819a60f2f451ba9f3f1
Rich FitzJohn <rich.fitzjohn@gmail.com>
2020-04-09 08:46:20 +0100

    New helper function for constant size arrays

--- inst/support.js
+++ inst/support.js
@@ -55,6 +55,70 @@ function getUser(user, name, internal, size, defaultValue,
     }
 }
 
+
+function getUserArray(user, name, internal, size, defaultValue,
+                      min, max, isInteger) {
+    var value = user[name];
+
+    if (isMissing(value)) {
+        if (isMissing(internal[name])) {
+            if (defaultValue === null) {
+                throw Error("Expected a value for '" + name + "'");
+            } else {
+                // not totally clear how to do this as we need to get
+                // the previous dimensions too!
+                throw Error("This needs implementing....");
+                internal[name] = defaultValue;
+            }
+        }
+        return;
+    }
+
+    var rank = size.length - 1;
+    if (!(typeof value === "object" && "data" in value && "dim" in value)) {
+        throw Error("Expected an odin.js array object for '" + name + "'");
+    }
+    if (value.dim.length !== rank) {
+        if (rank === 1) {
+            throw Error("Expected a numeric vector for '" + name + "'");
+        } else if (rank === 2) {
+            throw Error("Expected a numeric matrix for '" + name + "'");
+        } else {
+            throw Error("Expected a numeric array of rank " + rank +
+                        " for '" + name + "'");
+        }
+    }
+
+    for (var i = 0; i < rank; ++i) {
+        if (value.dim[i] !== size[i + 1]) {
+            if (rank == 1) {
+                throw Error("Expected length " + size[i + 1] +
+                            " value for '" + name + "'");
+            } else {
+                throw Error("Incorrect size of dimension " + (i + 1) +
+                            " of '" + name + "' (expected " + size[i + 1] +
+                            ")");
+            }
+        }
+    }
+
+    var data = value.data.slice();
+    var len = size[0];
+    for (var i = 0; i < len; ++i) {
+        if (typeof data[i] !== "number") {
+            throw Error("Expected a number for '" + name + "'");
+        }
+        if (min !== null && data[i] < min) {
+            throw Error("Expected '" + name + "' to be at least " + min);
+        }
+        if (max !== null && data[i] > min) {
+            throw Error("Expected '" + name + "' to be at most " + max);
+        }
+    }
+
+    internal[name] = data;
+}
+
 // With arrays there are really two ways that they might come in; on
 // the one hand we could accept json-style nested arrays, which is
 // cool, but requires considerable checking.  Or, given we want
@@ -100,7 +164,7 @@ function getUserArrayDim(user, name, internal, size, defaultValue,
         size[i + 1] = value.dim[i];
     }
 
-    data = value.data.slice();
+    var data = value.data.slice();
     for (var i = 0; i < len; ++i) {
         if (typeof data[i] !== "number") {
             throw Error("Expected a number for '" + name + "'");
--- tests/testthat/test-support.R
+++ tests/testthat/test-support.R
@@ -8,6 +8,77 @@ test_that("isMissing", {
 })
 
 
+test_that("getUserArray", {
+  ctx <- odin_js_support()
+  helper <- c(
+    "function test(user, name, size, defaultValue, min, max, isInteger) {",
+    "  var internal = {};",
+    "  getUserArray(user, name, internal, size, defaultValue, min, max,",
+    "               isInteger);",
+    "  return internal[name];",
+    "}")
+
+  ctx$eval(helper)
+
+  ## Get the required user data:
+  user <- list(a = jsonlite::unbox(1),
+               b = list(data = 1:6, dim = 2:3),
+               c = list(data = 1:4, dim = I(4)))
+  dim_b <- c(6, 2, 3)
+  dim_c <- c(4, 4)
+  dim_3 <- c(6, 2, 3, 1)
+
+  null <- V8::JS("null")
+  expect_equal(ctx$call("test", user, "b", dim_b, null, null, null, FALSE),
+               1:6)
+  expect_equal(ctx$call("test", user, "c", dim_c, null, null, null, FALSE),
+               1:4)
+
+  ## Check exists
+  expect_error(
+    ctx$call("test", user, "x", dim_b, null, null, null, FALSE),
+    "Expected a value for 'x'",
+    class = "std::runtime_error")
+
+  ## Throw if not a matrix-type object
+  expect_error(
+    ctx$call("test", user, "a", dim_b, null, null, null, FALSE),
+    "Expected an odin.js array object",
+    class = "std::runtime_error")
+
+  ## Check ranks
+  expect_error(
+    ctx$call("test", user, "b", dim_3, null, null, null, FALSE),
+    "Expected a numeric array of rank 3 for 'b'",
+    class = "std::runtime_error")
+  expect_error(
+    ctx$call("test", user, "b", dim_c, null, null, null, FALSE),
+    "Expected a numeric vector for 'b'",
+    class = "std::runtime_error")
+  expect_error(
+    ctx$call("test", user, "c", dim_b, null, null, null, FALSE),
+    "Expected a numeric matrix for 'c'",
+    class = "std::runtime_error")
+
+  ## Check is a number
+  user$b$data <- letters[1:6]
+  expect_error(
+    ctx$call("test", user, "b", dim_b, null, null, null, FALSE),
+    "Expected a number for 'b'",
+    class = "std::runtime_error")
+
+  ## Check range
+  expect_error(
+    ctx$call("test", user, "c", dim_c, null, 3, null, FALSE),
+    "Expected 'c' to be at least 3",
+    class = "std::runtime_error")
+  expect_error(
+    ctx$call("test", user, "c", dim_c, null, null, 3, FALSE),
+    "Expected 'c' to be at most 3",
+    class = "std::runtime_error")
+})
+
+
 test_that("getUserArrayDim", {
   ctx <- odin_js_support()
   helper <- c(

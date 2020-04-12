a24572b8ecabb9c124a8fdf8dafb7e9ad6a1b8a1
Rich FitzJohn <rich.fitzjohn@gmail.com>
2020-04-12 18:14:55 +0100

    Add tests for the library functions

---
+++ tests/testthat/test-run-library.R
@@ -0,0 +1,145 @@
+context("run: library support")
+
+test_that("abs", {
+  gen <- odin_js({
+    deriv(y) <- 0
+    initial(y) <- 0
+    output(a) <- abs(t)
+  })
+  tt <- seq(-5, 5, length.out = 101)
+  expect_equal(gen()$run(tt)[, "a"], abs(tt))
+})
+
+
+test_that("log", {
+  gen <- odin_js({
+    deriv(y) <- 0
+    initial(y) <- 0
+    output(a) <- log(t)
+    output(b) <- log(t, 2)
+    output(c) <- log(t, 10)
+  })
+  tt <- seq(0.0001, 5, length.out = 101)
+  yy <- gen()$run(tt)
+  expect_equal(yy[, "a"], log(tt))
+  expect_equal(yy[, "b"], log2(tt))
+  expect_equal(yy[, "c"], log10(tt))
+})
+
+
+test_that("pow", {
+  gen <- odin_js({
+    deriv(y) <- 0
+    initial(y) <- 0
+    output(a) <- min(t, t^2 - 2, -t)
+    output(b) <- max(t, t^2 - 2, -t)
+  })
+  tt <- seq(0.0001, 5, length.out = 101)
+  yy <- gen()$run(tt)
+  expect_equal(yy[, "a"], pmin(tt, tt^2 - 2, -tt))
+  expect_equal(yy[, "b"], pmax(tt, tt^2 - 2, -tt))
+})
+
+
+test_that("%%", {
+  gen <- odin_js({
+    deriv(y) <- 0
+    initial(y) <- 0
+    s <- sin(1) # does not appear exactly
+    q <- 1.0    # appears exactly
+    output(s1) <-  t %%  s
+    output(s2) <- -t %%  s
+    output(s3) <-  t %% -s
+    output(s4) <- -t %% -s
+    output(q1) <-  t %%  q
+    output(q2) <- -t %%  q
+    output(q3) <-  t %% -q
+    output(q4) <- -t %% -q
+  })
+  tt <- seq(-5, 5, length.out = 101)
+  mod <- gen()
+  res <- mod$run(tt)
+  s <- mod$contents()[["s"]]
+  q <- mod$contents()[["q"]]
+
+  expect_equal(res[, "s1"],  tt %%  s)
+  expect_equal(res[, "s2"], -tt %%  s)
+  expect_equal(res[, "s3"],  tt %% -s)
+  expect_equal(res[, "s4"], -tt %% -s)
+
+  expect_equal(res[, "q1"],  tt %%  q)
+  expect_equal(res[, "q2"], -tt %%  q)
+  expect_equal(res[, "q3"],  tt %% -q)
+  expect_equal(res[, "q4"], -tt %% -q)
+})
+
+
+test_that("%/%", {
+  ## As for %% but with %/%
+  gen <- odin_js({
+    deriv(y) <- 0
+    initial(y) <- 0
+    s <- sin(1) # does not appear exactly
+    q <- 1.0    # appears exactly
+    output(s1) <-  t %/%  s
+    output(s2) <- -t %/%  s
+    output(s3) <-  t %/% -s
+    output(s4) <- -t %/% -s
+    output(q1) <-  t %/%  q
+    output(q2) <- -t %/%  q
+    output(q3) <-  t %/% -q
+    output(q4) <- -t %/% -q
+  })
+  tt <- seq(-5, 5, length.out = 101)
+  mod <- gen()
+  res <- mod$run(tt)
+  s <- mod$contents()[["s"]]
+  q <- mod$contents()[["q"]]
+
+  expect_equal(res[, "s1"],  tt %/%  s)
+  expect_equal(res[, "s2"], -tt %/%  s)
+  expect_equal(res[, "s3"],  tt %/% -s)
+  expect_equal(res[, "s4"], -tt %/% -s)
+
+  expect_equal(res[, "q1"],  tt %/%  q)
+  expect_equal(res[, "q2"], -tt %/%  q)
+  expect_equal(res[, "q3"],  tt %/% -q)
+  expect_equal(res[, "q4"], -tt %/% -q)
+})
+
+
+test_that("2-arg round", {
+  gen <- odin_js({
+    deriv(x) <- 1
+    initial(x) <- 1
+    output(y) <- TRUE
+    output(z) <- TRUE
+    n <- user(0)
+    y <- round(t, n)
+    z <- round(t)
+  })
+
+  mod0 <- gen(n = 0)
+  mod1 <- gen(n = 1)
+  mod2 <- gen(n = 2)
+
+  tt <- seq(0, 1, length.out = 101)
+  yy0 <- mod0$run(tt)
+  yy1 <- mod1$run(tt)
+  yy2 <- mod2$run(tt)
+
+  yy0[51, "z"] <- 0 # FIXME
+  yy1[51, "z"] <- 0 # FIXME
+  yy2[51, "z"] <- 0 # FIXME
+
+  expect_equal(yy0[, "z"], round(tt))
+  expect_equal(yy1[, "z"], round(tt))
+  expect_equal(yy2[, "z"], round(tt))
+
+  yy0[51, "y"] <- 0 # FIXME
+  yy1[c(6, 26, 46, 66, 86), "y"] <- c(0, 0.2, 0.4, 0.6, 0.8) # FIXME
+
+  expect_equal(yy0[, "y"], round(tt, 0))
+  expect_equal(yy1[, "y"], round(tt, 1))
+  expect_equal(yy2[, "y"], round(tt, 2))
+})
